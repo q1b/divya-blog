04d6f2c06d80d8f34ebc2d6e89d049d35a0537cc
divyamrit <divyaisshiva@gmail.com>
2021-05-03 20:30:22 +0530

    second-change routes

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,43 +3,43 @@ let supabase = [
         id:1,
         date:'2nd March',
         topic:'Space',
-        content:''
+        content:'loremadsasdadad  adsada dads a sdas d sdkdagn'
     },
     {
         id:2,
         date:'2nd March',
         topic:'Space',
-        content:''
+        content:'loremadsasdadad  adsada dads a sdas d sdkdagn'
     },
     {
         id:3,
         date:'2nd March',
         topic:'Space',
-        content:''
+        content:'loremadsasdadad  adsada dads a sdas d sdkdagn'
     },
     {
         id:4,
         date:'2nd March',
         topic:'Space',
-        content:''
+        content:'loremadsasdadad  adsada dads a sdas d sdkdagn'
     },
     {
         id:5,
         date:'2nd March',
         topic:'Space',
-        content:''
+        content:'loremadsasdadad  adsada dads a sdas d sdkdagn'
     },
     {
         id:6,
         date:'2nd March',
         topic:'Space',
-        content:''
+        content:'loremadsasdadad  adsada dads a sdas d sdkdagn'
     },
     {
         id:7,
         date:'2nd March',
         topic:'Space',
-        content:''
+        content:'loremadsasdadad  adsada dads a sdas d sdkdagn'
     },
 ]
 
