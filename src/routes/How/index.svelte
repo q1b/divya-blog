f3486d1cb6149e4af002ece7f8402de098b19d15
divyamrit <divyaisshiva@gmail.com>
2021-05-05 22:53:54 +0530

    Modified Self, Modified Navbar Mobile view

--- src/routes/How/index.svelte
+++ src/routes/How/index.svelte
@@ -1,121 +1,120 @@
-<section class="w-full p-4 grid grid-cols-1 bg-white justify-items-center">
-  <article class="w-full font-jetMono p-2">
+<section class="grid w-full grid-cols-1 p-4 bg-white justify-items-center">
+  <article class="w-full p-2 sm:grid sm:grid-cols-1 sm:justify-items-center font-jetMono">
     <h1
-      class="text-white text-opacity-50 sm:w-max rounded-xl px-4 py-2 bg-black text-xl sm:text-2xl md:text-3xl "
+      class="px-4 py-2 text-white text-opacity-50 bg-black sm:text-xl md:text-2xl lg:text-3xl sm:w-max rounded-xl "
       style="text-shadow:1px 1px 0px #7700F0"
     >
-      How this all started ?
-    </h1>
+      How I got this all? </h1>
     <h1
-      class="mt-5 w-full px-5 py-1 rounded-lg text-white bg-[#00000070]"
+      class="mt-5 sm:w-max lg:text-2xl md:text-xl sm:text-lg w-full px-5 py-1 rounded-lg text-white bg-[#00000070]"
       style="box-shadow: inset #00000070 0px 0px 10px, 0px 0px 7px 6px #00000010;"
     >
       All this started as
     </h1>
     <h1
-      class="mt-7 w-full px-5 py-1 rounded-lg text-white bg-indigo-600"
+      class="w-full px-5 py-1 text-white bg-indigo-600 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg mt-7"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       Simple but elegant,
     </h1>
     <h1
-      class="mt-3 w-full px-5 py-1 rounded-lg text-white bg-pink-600"
+      class="w-full px-5 py-1 mt-3 text-white bg-pink-600 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       Difficult but cool,
     </h1>
     <h1
-      class="mt-3 w-full px-5 py-1 rounded-lg text-white bg-rose-600"
+      class="w-full px-5 py-1 mt-3 text-white rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg bg-rose-600"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       Awesome but joyful,
     </h1>
     <h1
-      class="mt-7 w-full px-5 py-1 rounded-lg text-white bg-orange-500"
+      class="w-full px-5 py-1 text-white bg-orange-500 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg mt-7"
       style="box-shadow: inset rgb(255 235 0) 0px 0px 10px, 0px 0px 7px 6px #00000010;"
     >
       _Now, It is Nothing
     </h1>
     <h1
-      class="mt-5 w-full px-5 py-1 rounded-lg text-white bg-yellow-400"
+      class="w-full px-5 py-1 mt-5 text-white bg-yellow-400 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       I am Everything But
     </h1>
     <h1
-      class="mt-3 w-full px-5 py-1 rounded-lg text-white bg-lime-400"
+      class="w-full px-5 py-1 mt-3 text-white rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg bg-lime-400"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       You might Not Trust
     </h1>
     <h1
-      class="mt-3  w-full px-5 py-1 rounded-lg text-white bg-green-400"
+      class="w-full px-5 py-1 mt-3 text-white bg-green-400 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       So, Why? I tell You
     </h1>
     <h1
-      class="mt-7 w-full px-5 py-1 rounded-lg text-white bg-teal-600"
+      class="w-full px-5 py-1 text-white bg-teal-600 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg mt-7"
       style="box-shadow: inset rgb(0 255 0) 0px 0px 10px, 0px 0px 7px 6px #00000010;"
     >
       About the things Of
     </h1>
     <h1
-      class="mt-5 w-full px-5 py-1 rounded-lg text-white bg-cyan-400"
+      class="w-full px-5 py-1 mt-5 text-white rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg bg-cyan-400"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       Reality Which I got
     </h1>
     <h1
-      class="mt-3 w-full px-5 py-1 rounded-lg text-white bg-light-blue-400"
+      class="w-full px-5 py-1 mt-3 text-white rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg bg-light-blue-400"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       By the Beyond Space
     </h1>
     <h1
-      class="mt-3  w-full px-5 py-1 rounded-lg text-white bg-blue-400"
+      class="w-full px-5 py-1 mt-3 text-white bg-blue-400 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       So, Why not you try
     </h1>
     <h1
-      class="mt-5  w-full px-5 py-1 rounded-lg text-white bg-purple-500"
+      class="w-full px-5 py-1 mt-5 text-white bg-purple-500 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       Go for it and merge
     </h1>
     <h1
-      class="mt-3  w-full px-5 py-1 rounded-lg text-white bg-fuchsia-400"
+      class="w-full px-5 py-1 mt-3 text-white rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg bg-fuchsia-400"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       With it with nature
     </h1>
     <h1
-      class="mt-3  w-full px-5 py-1 rounded-lg text-white bg-pink-400"
+      class="w-full px-5 py-1 mt-3 text-white bg-pink-400 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       It's Not You and Me
     </h1>
     <h1
-      class="mt-3  w-full px-5 py-1 rounded-lg text-white bg-rose-400"
+      class="w-full px-5 py-1 mt-3 text-white rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg bg-rose-400"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       But love that feels
     </h1>
     <h1
-      class="mt-3  w-full px-5 py-1 rounded-lg text-white bg-red-400"
+      class="w-full px-5 py-1 mt-3 text-white bg-red-400 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       Everything by heart
     </h1>
     <h1
-      class="mt-3  w-full px-5 py-1 rounded-lg text-white bg-orange-400"
+      class="w-full px-5 py-1 mt-3 text-white bg-orange-400 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       Neither Me nor You,
     </h1>
     <h1
-      class="mt-3  w-full px-5 py-1 rounded-lg text-orange-900 bg-amber-400"
+      class="w-full px-5 py-1 mt-3 text-orange-900 rounded-lg  sm:w-max lg:text-2xl md:text-xl sm:text-lg bg-amber-400"
       style="box-shadow: 0px 5px 15px 0px #00000050;"
     >
       But It's Everything
--- src/routes/Self/index.svelte
+++ src/routes/Self/index.svelte
@@ -1,3 +1,25 @@
-<script></script>
 
-<h1>Self my intoduction</h1>
+<div class="grid w-full grid-cols-1 pt-10 pb-64 font-semibold bg-black justify-items-center bg-opacity-95 font-jetMono">
+  <article class="w-11/12 rounded-2xl">
+    <h1 class="text-transparent sha-white typo bg-gradient-to-tr from-cyan-200 to-white bg-clip-text">
+      <big class="text-4xl md:text-6xl sm:text-5xl">M</big>y self Divya, My dream is very uncertain and unexpected for many peoples, My dream is to become everything.
+      <br><br>I am made up of What ?</h1>
+  </article>
+  <article class="w-11/12 p-4 mt-3 mb-10 bg-black  sm:mb-12 md:mb-24 rounded-xl">
+    <h1 class="text-sm leading-7 text-white md:text-xl sm:text-lg">Mass particles which are called Atoms or The Things of which Atoms are made up of or That 
+      <mark class="Mark px-0.5" 
+            style="background-image: linear-gradient(90deg, hsla(207, 100%, 13%, 1) 0%, hsla(354, 93%, 37%, 1) 30%, hsla(51, 100%, 150%, 0.5) 50%, hsla(180, 200%, 50%, 1) 100%)"
+      >
+        thing from which everything
+      </mark>
+      <mark class="px-0.5 Mark"
+            style="background-image: linear-gradient(90deg, hsla(240, 200%, 70%, 1) 0%, hsla(27, 30%, 50%, 1) 50%, hsla(174, 100%, 50%, 1) 100%);"
+      >is made up</mark> of made up of which thing, Then 
+       the thing from which
+        that thing is made up of,
+       Is the answer . Yes I am in search of that thing which is source and main building block of this reality,Yet at present day researcher are doing experiments, in order to understand Nature But Everytime Experiments cannot work because we can do experiments on that thing which we can see in this physical realm but there are many other dimension, In order to give you a hint, Think about yourself , What is the mechanisim before your thinking patterns ? </h1>
+  </article>
+  <article class="px-5 py-10 bg-white mt-7">
+    <h1 class="text-transparent sha-Graywhite typo bg-gradient-to-tr from-cyan-700 to-blueGray-800 bg-clip-text"><big class="text-4xl">I</big> want to understand whole this reality and want to know how this reality really works and how it manages to create such beautiful objects, You can say that I want to become scientist in simple words but I want to become a man who pertinence to the knowlegde of universe.<br><br>I know it's might be seem impossible for anyone but when I see our past then where we are standing in present seems impossible for our ancestors also but Now, We can reach moon,planets and explore space.<br><br>I need no money from anyone in this path but I need only good friends.</h1>
+  </article>
+</div>
